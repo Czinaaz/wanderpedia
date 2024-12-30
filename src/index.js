355d9b21483716b503e73f302dcfa9d6e9c88bff
Ivan Rudenko <rudenko.ivan81@gmail.com>
2024-12-30 14:20:05 +0100

    save

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store';
 
-import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));  
+ReactDOM.createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+    <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/wanderpedia/">
+        <App />
+        </BrowserRouter>
+    </PersistGate>
+    </Provider>
+</React.StrictMode>
+);
